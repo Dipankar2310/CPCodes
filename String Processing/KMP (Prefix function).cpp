2587625288c3feb09615a5a8428b98f6673f4b73
Dipankar2310 <Dipankarchaudhary8@gmail.com>
2022-08-10 22:44:53 +0530

    Updated prefix function

--- String Processing/KMP (Prefix function).cpp
+++ String Processing/KMP (Prefix function).cpp
@@ -15,14 +15,18 @@ const ll INF = 1e9;
  
 // f[i] = length of the longest proper prefix of the substring s[0...i] which is also a suffix of this substring
 vector<int> prefix_func(string s) {
-    int n = s.size();
-    vector<int> f(n); 
+    int n = (int)s.length();
+    vector<int> pi(n);
     for (int i = 1; i < n; i++) {
-        int j = f[i - 1];
-        while (j && s[i] != s[j]) j = f[j - 1];
-        f[i] = j + (s[i] == s[j]);
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j-1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
     }
-    return f;
+    return pi;
+
 }
  
 int cnt_occ(string s, string t) {
