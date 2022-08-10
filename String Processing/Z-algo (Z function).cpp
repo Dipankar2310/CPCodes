d2438be1d2fb265b9ab72083a94368b3f816261e
Dipankar2310 <Dipankarchaudhary8@gmail.com>
2022-08-10 22:48:43 +0530

    Update Z-algo (Z function).cpp

--- String Processing/Z-algo (Z function).cpp
+++ String Processing/Z-algo (Z function).cpp
@@ -15,14 +15,17 @@ const ll INF = 1e9;
  
 // f[i] = length of the longest common prefix between s and the substring s[i...n-1]
 vector<int> z_func(string s) {
-    int n = s.size();
-    vector<int> f(n); // f[0] = 0 by default
-    for (int i = 1, l = 0, r = 0; i < n; i++) {
-        if (i <= r) f[i] = min(r - i + 1, f[i - l]);
-        while (i + f[i] < n && s[f[i]] == s[i + f[i]]) f[i]++;
-        if (i + f[i] - 1 > r) l = i, r = i + f[i] - 1;
+   int n = (int) s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
     }
-    return f;
+    return z;
 }
  
 int cnt_occ(string s, string t) {
